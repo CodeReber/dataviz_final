b0d70cf5de135aa833f8c6ab603148a0389b4b19
codereber <jamesmpyle@gmail.com>
2020-11-14 07:43:26 -0500

    update

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import requests
 from pprint import pprint
 
 parameters = {
-    "Authorization": '5897db64d0634bc43bfbfffb738fd1b7ed0104da'
+    "lat": '39.95',
+    "lon": '-75.16',
+    "limit": '10'
 }
 
-url = "https://app.climate.azavea.com/api/scenario/"
+url = "https://app.climate.azavea.com/api/city/nearest/"
 
-response = requests.get(url, headers={'Authorization': 'Token 5897db64d0634bc43bfbfffb738fd1b7ed0104da'})
+response = requests.get(url, params=parameters, headers={'Authorization': 'Token 5897db64d0634bc43bfbfffb738fd1b7ed0104da'})
 
 
 pprint(response.json())
