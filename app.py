b1440557a085dabd6922a726a975c4cb250ba5b8
pprasitanond <pprasitanond1@babson.edu>
2020-11-18 09:25:58 +0700

    html commit

--- app.py
+++ app.py
@@ -9,25 +9,25 @@ model = pickle.load(open('model.pkl', 'rb'))
 def index():
     return render_template('index.html')
 
-@app.route('/clean')
-def clean():
-    return render_template('clean.html')
+@app.route('/cleaning')
+def cleaning():
+    return render_template('cleaning.html')
 
-@app.route('/ml')
-def ml():
-    return render_template('ml.html')
+@app.route('/machine_learning')
+def machine_learning():
+    return render_template('machine_learning.html')
 
 @app.route('/map')
 def map():
     return render_template('map.html')
 
-@app.route('/graphs')
-def graphs():
-    return render_template('graphs.html')
+@app.route('/graph')
+def graph():
+    return render_template('graph.html')
 
-@app.route('/results')
-def results():
-    return render_template('results.html')
+@app.route('/result')
+def result():
+    return render_template('result.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
