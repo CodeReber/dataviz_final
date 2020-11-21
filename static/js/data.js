d78912e6987032cec620ca7287807ef49251d8b6
Asiha Braxton-Garvin <asiha14@gmail.com>
2020-11-21 13:06:12 -0500

    adding data table

--- static/js/data.js
+++ static/js/data.js
@@ -1,61 +1,106 @@
 var select = d3.select("select")
-var rats = ["","Dens","1985-2016","2009-2016","Climate"]
-rats.forEach( d =>{
+var data_tables = ["","Dens","1985-2016","2009-2016","Climate"]
+data_tables.forEach( d =>{
     var opt = select.append("option");
     opt.property("value",d);
     opt.text(d);
 
 });
 
-console.log("am I working")
 
-d3.json("/api/dens")
-  .then(function (data) {
-    console.log(data);
-    // Do some error handling.
-    function tabulate(data, columns) {
-      var table = d3.select("table");
-      var thead = table.select("thead");
-      var tbody = table.select("tbody");
 
-      // append the header row
-      thead
-        .append("tr")
-        .selectAll("th")
-        .data(columns)
-        .enter()
-        .append("th")
-        .text(function (column) {
-          return column;
-        });
+select.on("change",runEnter)
+var columns = []
 
-      // create a row for each object in the data
-      var rows = tbody.selectAll("tr").data(data).enter().append("tr");
+function runEnter(){
+    var dropdownMenu = d3.select("select");
+        // Assign the value of the dropdown menu option to a variable
+    var option = dropdownMenu.property("value");
 
-      // create a cell in each row for each column
-      var cells = rows
+    if (option === "Dens"){
+        dens()
+        columns = ['record','denid','spring_year','data_source','discovery_method','latitude','longitude','confirmation','substrate','position_method','horizontal_error']
+    }
+    else if (option === "1985-2016"){
+        eightyfivesixteen()
+        columns = ['BearID_rsf','DateTimeUTC_rsf','latitude_rsf','longitude_rsf','season','period','lc94_rsf','eaInterval_rsf']
+    }
+    else if (option === "2009-2016"){
+        ninesixteen()
+        columns = ['BearId','GmtDate','GmtTime','longitude','latitude','Raw_Act','Standard_act','Active_Den','Habitat']
+    }
+    else if(option === "Climate"){
+        climate()
+        columns = ['Year_Month','Land_Avg_Temp','Land_Max_Temp','Land_Min_Temp','Land_Ocean_Avg_Temp',
+        'North_Min_Temp_Anomoly', 'North_Max_Temp_Anomoly','North_Mean_Temp_Anomoly']
+    };
+    
+};
+
+var theads =  d3.select('thead')
+var tbody =  d3.select('tbody')
+
+function createTables(data, columns){
+    console.log(data.results)
+
+    d3.selectAll("td").remove()
+    d3.selectAll("tr").remove()
+        
+
+    var tr = theads.append("tr")
+    columns.forEach(c =>{        
+        var td=tr.append("td")
+        td.text(c)
+    });
+    var results = data.results
+
+    var rows = tbody.selectAll("tr").data(results).enter().append("tr");
+
+    // create a cell in each row for each column
+    var cells = rows
         .selectAll("td")
         .data(function (row) {
-          return columns.map(function (column) {
+        return columns.map(function (column) {
             return { column: column, value: row[column] };
-          });
+        });
         })
         .enter()
         .append("td")
         .text(function (d) {
-          return d.value;
+        return d.value;
         });
 
-      return table;
-    }
+}
 
-    var col = ['record','denid','spring_year','data_source','discovery_method','latitude','longitude','confirmation','substrate','position_method','horizontal_error_m'
-]
-    // render the table(s)
-    tabulate(data, col );
+function dens(){
+    d3.json("/api/dens")
+    .then((data) => {
+        createTables(data,columns)
+    });
 
-    // Do some error handling.
-  })
-  .catch(function (error) {
-    // Do some error handling.
-  });
+};
+
+function climate(){
+    d3.json("/api/climate")
+    .then((data)=>{
+        createTables(data,columns)
+    });
+
+};
+
+function ninesixteen(){
+    d3.json('/api/bears_2009_2016')
+    .then((data)=>{
+        createTables(data,columns)
+    });
+
+};
+
+function eightyfivesixteen(){
+    d3.json('/api/bears_1985_2016')
+    .then((data)=>{
+        createTables(data,columns)
+     
+    });
+
+};
